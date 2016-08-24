05e6bcf7068064613a6af49588187517d6748ec7
Gagaro <gagaro42@gmail.com>
2016-08-24 20:38:28 +0200

    initial commit

---
+++ Source/FairerMood/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Gagaro
+ * Date: 24/08/2016
+ * Time: 20:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace FairerMood
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
